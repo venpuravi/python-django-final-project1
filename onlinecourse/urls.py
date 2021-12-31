86ea0f72ffefc3a201b1888d076848e012d83c4c
balad <venpuravi@gmail.com>
2021-12-31 20:49:28 +0000

    submit view 1

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
     # ex: /enroll/5/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
+    path('<int:course_id>/submit/', views.submit, name='submit'),
 
     # <HINT> Create a route for submit view
 
